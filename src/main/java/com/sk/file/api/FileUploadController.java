ee652df60d083fbdda6f532f5bf938389880ba98
190726 <52764723+190726@users.noreply.github.com>
2023-04-18 12:03:34 +0900

    simple file upload
    client use pure javascript fetch api

--- src/main/java/com/sk/file/api/FileUploadController.java
+++ src/main/java/com/sk/file/api/FileUploadController.java
@@ -1,10 +1,41 @@
 package com.sk.file.api;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/file")
 public class FileUploadController {
+	
+	@PostMapping("/upload1")
+	public Response upload1(
+			@RequestParam(value = "message") String message,
+			@RequestParam(value = "file", required = false) MultipartFile file) {
+		System.out.println(message);
+		System.out.println(file.getName());
+		System.out.println(file.getSize());
+		System.out.println(file.getResource());
+		return new Response("success");
+	}
+	
+	static class Response{
+		private String message;
 
+		public String getMessage() {
+			return message;
+		}
+
+		public void setMessage(String message) {
+			this.message = message;
+		}
+
+		public Response(String message) {
+			super();
+			this.message = message;
+		}
+		
+	}
 }
