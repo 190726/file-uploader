f28c2681048e6c80ca6c99147ecbb865e4fea180
190726 <52764723+190726@users.noreply.github.com>
2023-04-18 10:37:12 +0900

    first commit

---
+++ src/main/java/com/sk/file/api/FileUploadController.java
@@ -0,0 +1,10 @@
+package com.sk.file.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/file")
+public class FileUploadController {
+
+}
